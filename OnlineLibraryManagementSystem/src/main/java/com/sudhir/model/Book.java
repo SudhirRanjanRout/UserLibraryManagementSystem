5d86f49a631212a25b4fd73278a54f8df3464070
Sudhir.Rout <Sudhir.Rout@IN2228372W1.ey.net>
2017-09-07 12:23:16 +0530

    1st commit

---
+++ OnlineLibraryManagementSystem/src/main/java/com/sudhir/model/Book.java
@@ -0,0 +1,23 @@
+/*package com.sudhir.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Books_Store")
+public class Book {
+	@Id
+	@Column(name = "bookId")
+	private Long bookId;
+	@Column(name = "bookName")
+	private String bookName;
+	@Column(name = "isbn10")
+	private Long isbn10;
+	@Column(name = "isbn13")
+	private Long isbn13;
+	@Column(name = ")
+
+}
+*/
